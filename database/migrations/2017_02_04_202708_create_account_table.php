91990f988070439e144d9192b1dd8af809ad8eae
Victor Lam <victorebox@yahoo.com.hk>
2017-02-05 14:00:33 +0800

    migration for account

---
+++ database/migrations/2017_02_04_202708_create_account_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->bigInteger('balance')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account');
+    }
+}
