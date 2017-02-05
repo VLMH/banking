2089f588879f60fe28f5b97ef3257e9e6410af85
Victor Lam <victorebox@yahoo.com.hk>
2017-02-05 13:59:32 +0800

    create model account with association with user

--- app/User.php
+++ app/User.php
@@ -8,4 +8,14 @@ class User extends Model
 {
     protected $table = 'user';
     protected $fillable = ['hkid'];
+
+    public function accounts()
+    {
+        return $this->hasMany('App\Account');
+    }
+
+    public function activeAccounts()
+    {
+        return $this->accounts()->whereNull('deleted_at');
+    }
 }
