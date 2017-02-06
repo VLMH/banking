f351beadc9ca857bb78a1dd515b8afe5c0c3ff7d
Victor <victorboxster@gmail.com>
2017-02-06 11:31:48 +0800

    make use of api middleware

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::get('/users', 'UserController@index');
+Route::post('/users', 'UserController@create');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/users/{userId}/accounts', 'AccountController@index');
+Route::get('/users/{userId}/accounts/{accountId}', 'AccountController@show');
+Route::post('/users/{userId}/accounts', 'AccountController@create');
+Route::delete('/users/{userId}/accounts/{accountId}', 'AccountController@destroy');
+Route::post('/users/{userId}/accounts/{accountId}/deposit', 'AccountController@deposit');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return [
+      'user' => [
+        'GET /users',
+        'GET /users/{id}',
+        'POST /users',
+      ],
+      'account' => [
+        'GET /users/{id}/accounts',
+        'GET /users/{id}/accounts/{accountId}',
+        'POST /users/{id}/accounts',
+        'DELETE /users/{id}/accounts/{accountId}',
+        'POST /users/{id}/accounts/{accountId}/deposit',
+        'POST /users/{id}/accounts/{accountId}/withdraw',
+        'POST /users/{id}/accounts/{accountId}/transfer',
+      ]
+    ];
 });
